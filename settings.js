362d0c1c8a30a9bb299242026ddb8bdd3dfd6f49
۝⃟̥̩̩̩̥̩̥͚̮♥නිමේෂ♥😝™ࣩ࿔ࣰ🎓❁ཻ͜͡彡😄꧁⃟⃞❀༂❃⃟𝙉𝙄⃞𝙈𝙀𝙎⃟𝙃𝘼❃❀⃟꧂۝⃟̥̩̩̩̥̩̥͚̮ <121599526+nimesha206@users.noreply.github.com>
2026-01-06 21:25:32 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,19 @@
+const settings = {
+  packname: 'ᴄʜᴜᴛɪ ᴡᴀ ʙᴏᴛ',
+  author: 'ɴɪᴍᴇsʜᴀ ᴍᴀᴅʜᴜsʜᴀɴ',
+  botName: "ᴄʜᴜᴛɪ ᴡᴀ ʙᴏᴛ",
+  botOwner: 'ɴɪᴍᴇsʜᴀ ᴍᴀᴅʜᴜsʜᴀɴ', // Your name
+  ownerNumber: '94726800969', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "👥public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "> ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ: මෙම බොට් සමූහ කළමනාකරණය සහ කාර්යයන් ස්වයංක්‍රීය කිරීම සඳහා නිර්මාණය කර ඇත.",
+  caption: "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴɪᴍᴇsʜᴀ ᴍᴀᴅʜᴜsʜᴀɴ",
+  version: "1.0",
+  prefix: ".", // prefix (e.g., ., /, !, *)
+  timezone: "Asia/Colombo", //put your country timeZone....leave blank if u don't know
+  updateZipUrl: "https://github.com/nimesha206/CHUTI.git",
+};
+
+module.exports = settings;
